a9e028dfeb99da3be7c5d6771a3540b36ce7cc2f
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-11 21:09:08 +0300

    Реализация API клиента

---
+++ SmartWeatherBot/Weather/IOpenWeatherMapClient.cs
@@ -0,0 +1,13 @@
+﻿using SmartWeatherBot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartWeatherBot.Weather
+{
+    public interface IOpenWeatherMapClient
+    {
+        Task<WeatherData> GetWeatherAsync(double lat, double lon);
+    }
+}
