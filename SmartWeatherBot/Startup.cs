e153aad640dc3361051092230d4222e455521b38
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-11 20:25:59 +0300

    очистка кода

--- SmartWeatherBot/Startup.cs
+++ SmartWeatherBot/Startup.cs
@@ -27,7 +27,6 @@ namespace SmartWeatherBot
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             FluentMapper.Initialize(config =>
@@ -48,7 +47,6 @@ namespace SmartWeatherBot
                 .AddJsonOptions(options => { options.JsonSerializerOptions.IgnoreNullValues = true; });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
