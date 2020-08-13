a379da3f50a575126cb7762f0aa89d60757cc92d
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-13 21:53:30 +0300

    реализация методов

--- SmartWeatherBot/Bot/TelegramHandler.cs
+++ SmartWeatherBot/Bot/TelegramHandler.cs
@@ -25,26 +25,33 @@ namespace SmartWeatherBot.Bot
 
         private void OnMessage(Message Message)
         {
+            CheckUser(Message.Chat.Id).Wait();
             switch (Message.Type)
             {
                 case Telegram.Bot.Types.Enums.MessageType.Text:
                     var action = (Message.Text.Split(' ').First()) switch
                     {
+                        "/start" => OnReceiveStart(Message.Chat.Id, Message.Chat.Username),
+                        "/weather" => OnReceiveWeather(Message.Chat.Id),
+                        "/location" => OnReceiveLocation(Message.Chat.Id),
+                        "/update" => OnReceiveUpdateRequest(Message.Chat.Id),
                         _ => OnReceiveUnknown(Message.Chat.Id)
                     };
+                    action.Wait();
                     break;
                 case Telegram.Bot.Types.Enums.MessageType.Location:
 
                     break;
                 default:
-
+                    OnReceiveUnknown(Message.Chat.Id).Start();
                     break;
             }
         }
 
-        protected abstract Task OnReceiveStart(long chatId);
+        protected abstract Task CheckUser(long chatId);
+        protected abstract Task OnReceiveStart(long chatId, string username);
         protected abstract Task OnReceiveLocation(long chatId);
-        protected abstract Task OnReceiveGetWeather(long chatId);
+        protected abstract Task OnReceiveWeather(long chatId);
         protected abstract Task OnReceiveUpdateRequest(long chatId);
         protected abstract Task OnReceiveUnknown(long chatId);
         protected abstract Task SendAsync(long chatId, string message);
--- SmartWeatherBot/ReceiveHandler.cs
+++ SmartWeatherBot/ReceiveHandler.cs
@@ -1,5 +1,6 @@
 ﻿using SmartWeatherBot.Bot;
 using SmartWeatherBot.Database;
+using SmartWeatherBot.Models;
 using SmartWeatherBot.Weathers;
 using System;
 using System.Collections.Generic;
@@ -21,9 +22,41 @@ namespace SmartWeatherBot
             _users = repository;
         }
 
-        protected override Task OnReceiveGetWeather(long chatId)
+        protected override async Task CheckUser(long chatId)
         {
-            throw new NotImplementedException();
+            var user = await _users.GetUserTelegram(chatId);
+            if(user == null)
+            {
+                user = new User()
+                {
+                    TelegramID = chatId
+                };
+                await _users.InsertAsync(user);
+            }
+        }
+
+        protected override async Task OnReceiveWeather(long chatId)
+        {
+            var user = await _users.GetUserTelegram(chatId);
+            if(user != null)
+            {
+                if (user.IsValidLocation())
+                {
+                    var weatherStatus = await _weather.GetWeatherAsync(user.Lat, user.Lon);
+                    if(weatherStatus != null)
+                    {
+                        await SendAsync(chatId, $"Текущая температура: {weatherStatus.Temp}\r\nОщущается как: {weatherStatus.TempLike}");
+                    }
+                    else
+                    {
+                        await SendAsync(chatId, $"Что то пошло не так, попробуйте позже");
+                    }
+                }
+                else
+                {
+                    await SendAsync(chatId, "к сожалению ваша геолокация неизвестна. Сообщите мне ваши координаты и тогда я смогу вам сообщить погоду!");
+                }
+            }
         }
 
         protected override Task OnReceiveLocation(long chatId)
@@ -31,14 +64,14 @@ namespace SmartWeatherBot
             throw new NotImplementedException();
         }
 
-        protected override Task OnReceiveStart(long chatId)
+        protected override async Task OnReceiveStart(long chatId, string username)
         {
-            throw new NotImplementedException();
+            await SendAsync(chatId, $"Приветствую тебя, {username}!\r\n Я умный погодный Telegram бот. \r\nЯ подскажу тебе погоду по твоему местоположению!");
         }
 
-        protected override Task OnReceiveUnknown(long chatId)
+        protected override async Task OnReceiveUnknown(long chatId)
         {
-            throw new NotImplementedException();
+            await SendAsync(chatId, "Я не понимаю тебя, человек");
         }
 
         protected override Task OnReceiveUpdateRequest(long chatId)
@@ -46,9 +79,9 @@ namespace SmartWeatherBot
             throw new NotImplementedException();
         }
 
-        protected override Task SendAsync(long chatId, string message)
+        protected override async Task SendAsync(long chatId, string message)
         {
-            throw new NotImplementedException();
+            await _bot.Bot.SendTextMessageAsync(chatId, message);
         }
     }
 }
