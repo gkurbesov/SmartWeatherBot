e8ab15e892c3ac57edbe0f84aa4702deaebcd9e4
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-13 20:59:37 +0300

    классы для работы с данными

---
+++ SmartWeatherBot/HostService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Hosting;
+using SmartWeatherBot.Bot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartWeatherBot
+{
+    public class HostService : BackgroundService
+    {
+        private readonly TelegramHandler _telegram;
+
+
+        public HostService(TelegramHandler telegramHandler)
+        {
+            _telegram = telegramHandler;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(5000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ SmartWeatherBot/ReceiveHandler.cs
@@ -0,0 +1,54 @@
+﻿using SmartWeatherBot.Bot;
+using SmartWeatherBot.Database;
+using SmartWeatherBot.Weathers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartWeatherBot
+{
+    public class ReceiveHandler : TelegramHandler
+    {
+        IWeatherService _weather;
+        IUserRepository _users;
+
+        public ReceiveHandler(ITelegramBot telegramBot, 
+            IWeatherService weatherService,
+            IUserRepository repository) : base(telegramBot)
+        {
+            _weather = weatherService;
+            _users = repository;
+        }
+
+        protected override Task OnReceiveGetWeather(long chatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnReceiveLocation(long chatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnReceiveStart(long chatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnReceiveUnknown(long chatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task OnReceiveUpdateRequest(long chatId)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task SendAsync(long chatId, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
