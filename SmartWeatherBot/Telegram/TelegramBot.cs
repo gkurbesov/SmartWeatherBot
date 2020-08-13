e2939e013fbd02ad1d1418117bbdfd824a15fe7e
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-13 18:48:15 +0300

    Добавил классы для работы с Telegram

---
+++ SmartWeatherBot/Telegram/TelegramBot.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace SmartWeatherBot.Telegram
+{
+    public class TelegramBot : ITelegramBot
+    {
+        public TelegramBotClient Bot { get; set; }
+
+        public TelegramBot(IOptions<TelegramConfig> options)
+        {
+            try
+            {
+                Bot = new TelegramBotClient(options.Value.Token);
+                ClietnSetCommand();
+            }
+            catch (Exception ex)
+            {
+                Bot = null;
+            }
+        }
+
+        private void ClietnSetCommand()
+        {
+            var commands = new BotCommand[]
+            {
+                 new BotCommand(){Command = "/status", Description = "Получить текущую температуру"},
+                 new BotCommand(){Command = "/location", Description = "Изменить гео-локацию для получения прогноза"},
+                 new BotCommand(){Command = "/news", Description = "Сообщать свежий прогноз погоды"},
+            };
+            Bot?.SetMyCommandsAsync(commands);
+        }
+
+    }
+}
---
+++ SmartWeatherBot/Telegram/TelegramHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace SmartWeatherBot.Telegram
+{
+    public class TelegramHandler
+    {
+        private readonly ITelegramBot _bot;
+
+        public TelegramHandler(ITelegramBot telegramBot)
+        {
+            _bot = telegramBot;
+            if (_bot.Bot != null)
+                InitHandler();
+        }
+
+        private void InitHandler()
+        {
+            _bot.Bot.OnMessage += (s, e) => OnMessage(e.Message);
+            _bot.Bot.StartReceiving();
+        }
+
+        private void OnMessage(Message Message)
+        {
+           
+        }
+    }
+}
